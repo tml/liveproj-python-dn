6c319ef532377f576976bcbc53240c727d14c716
Joey Smith <joeysmith@gmail.com>
2020-05-24 17:07:44 -0600

    simplify logic by using list extend

--- dn.py
+++ dn.py
@@ -5,7 +5,7 @@ import sys
 def getfiles():
     found = []
     for root, dirnames, filenames in os.walk(os.getcwd()):
-        found.append(''.join([item for sublist in filenames for item in sublist if sublist.endswith('.pdf')]))
+        found.extend([item for item in filenames if item.endswith(".pdf")])
     return [file for file in found if len(file)]
 
 
