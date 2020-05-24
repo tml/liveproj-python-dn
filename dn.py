9559fa4a0caae67a4c488b07fa0beab724f070a8
Joey Smith <joeysmith@gmail.com>
2020-05-24 17:07:27 -0600

    black formatter

--- dn.py
+++ dn.py
@@ -1,11 +1,13 @@
 import os
 import sys
 
+
 def getfiles():
     found = []
     for root, dirnames, filenames in os.walk(os.getcwd()):
         found.append(''.join([item for sublist in filenames for item in sublist if sublist.endswith('.pdf')]))
     return [file for file in found if len(file)]
+
 
 if __name__ == "__main__":
     print(getfiles())
